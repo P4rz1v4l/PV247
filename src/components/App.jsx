bfa26444b7af650cbfa3af8d6ae4401650aa21a7
DESKTOP-7BHA77P\lukkr <luk.krizan@gmail.com>
2018-10-15 19:24:27 +0200

    Menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAlignLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAlignLeft, faComments } from '@fortawesome/free-solid-svg-icons';
 
 import { Menu } from './menu/Menu';
 import { Chat } from './chat/Chat';
 
 import './app.scss';
 
-library.add(faAlignLeft);
+library.add(faAlignLeft, faComments);
 
 export class App extends React.PureComponent {
   render() {
--- src/components/menu/MenuChannels..jsx
+++ src/components/menu/MenuChannels..jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export class MenuChannels extends React.PureComponent {
   render() {
     return (
@@ -11,7 +13,50 @@ export class MenuChannels extends React.PureComponent {
           <span>+</span>
         </div>
         <div className="list">
-          <div className="channel"></div>
+          <div className="d-flex align-items-stretch justify-content-between channel">
+            <div>
+              <div className="d-flex align-items-start name">
+                <FontAwesomeIcon icon="comments" />
+                <div>
+                  <h3>name2</h3>
+                  <span>1 new message</span>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex align-items-center">
+              ...
+            </div>
+          </div>
+
+          <div className="d-flex align-items-stretch justify-content-between channel">
+            <div>
+              <div className="d-flex align-items-start">
+                <FontAwesomeIcon icon="comments" />
+                <div>
+                  <h3>name2</h3>
+                  <span>1 new message</span>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex align-items-center">
+              ...
+            </div>
+          </div>
+
+          <div className="d-flex align-items-stretch justify-content-between channel active">
+            <div>
+              <div className="d-flex align-items-start">
+                <FontAwesomeIcon icon="comments" />
+                <div>
+                  <h3>name2</h3>
+                  <span>1 new message</span>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex align-items-center">
+              ...
+            </div>
+          </div>
         </div>
       </div>
     );
